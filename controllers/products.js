d3cec837ec1a7b3306cc4acc7394d670a1e9ed03
Jack Trevor <sbrobools@gmail.com>
2024-01-30 17:43:50 -0300

    Installed Swagger

--- controllers/products.js
+++ controllers/products.js
@@ -2,6 +2,7 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
+    //#swagger.tags=['Products']
     const result = await mongodb.getDatabase().db().collection('products').find();
     result.toArray().then((products) => {
         res.setHeader('Content-Type', 'application/json');
@@ -11,6 +12,7 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
+    //#swagger.tags=['Products']
     const productId = new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().db().collection('products').find({_id: productId});
     result.toArray().then((products) => {
@@ -19,7 +21,53 @@ const getSingle = async (req, res) => {
     }); 
 };
 
+const createProduct = async (req, res) => {
+    //#swagger.tags=['Products']
+    const product = {
+        productName: req.body.productName,
+        brand: req.body.brand,
+        amount: req.body.amount,
+        price: req.body.price
+    };
+    const response = await mongodb.getDatabase().db().collection('products').insertOne(product);
+    if (response.acknowledged) {
+        res.status(201).send(response);
+    } else {
+        res.status(500).json(response.error || 'Some error ocurred while updating the user.');
+    }
+};
+const updateProduct = async (req, res) => {
+    //#swagger.tags=['Products']
+    const productId = new ObjectId(req.params.id);
+    const product = {
+        productName: req.body.productName,
+        brand: req.body.brand,
+        amount: req.body.amount,
+        price: req.body.price
+    };
+    const response = await mongodb.getDatabase().db().collection('products').replaceOne({ _id: productId}, product);
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error ocurred while updating the user.');
+    }
+};
+
+const deleteProduct = async (req, res) => {
+    //#swagger.tags=['Products']
+    const productId = new ObjectId(req.params.id);
+    const response = await mongodb.getDatabase().db().collection('products').deleteOne({ _id: productId});
+    if (response.deletedCount > 0) {
+        res.status(204). send();
+    } else {
+        res.status(500).json(response.error || 'Some error ocurred while deleting the user.');
+    }
+};
+
 module.exports = {
     getAll,
     getSingle,
+    createProduct,
+    updateProduct,
+    deleteProduct
 };
--- routes/products.js
+++ routes/products.js
@@ -7,4 +7,12 @@ router.get('/', productsController.getAll);
 
 router.get('/:id', productsController.getSingle);
 
+router.post('/', productsController.createProduct);
+
+router.put('/:id', productsController.updateProduct);
+
+router.delete('/:id', productsController.deleteProduct);
+
+
 module.exports = router;
+
